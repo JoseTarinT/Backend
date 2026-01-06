dadf3d32311546bb21295aa2cad015d8e79b0487
JoseTarinT <joseluis_tt88@hotmail.com>
2026-01-06 14:44:00 +1100

    Add code fixes and improvements

--- index.js
+++ index.js
@@ -1,51 +1,70 @@
-const express = require("express")
-const puppeteer = require('puppeteer')
-const { Cluster } = require("puppeteer-cluster")
-const cors = require('cors')
-const app = express()
-app.use(cors())
+const express = require("express");
+const puppeteer = require("puppeteer");
+const { Cluster } = require("puppeteer-cluster");
+const cors = require("cors");
+require("dotenv").config();
+const axios = require("axios");
+const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.json('Hello')
-})
+app.get("/", async (req, res) => {
+  try {
+    const { data } = await axios.get(process.env.API);
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
 
-app.get('/result', (req, res) => {
-    async function scrapeTemperature() {
-        const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-        const page = await browser.newPage();
-        await page.goto('https://tides4fishing.com/au/new-south-wales/merimbula#_water_temp');
-    
-        const tmp = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll('#c_grafico_temp_agua_txt_grados_agua_actual strong')).map(x => x.textContent)
-        });
-        
-        //console.log({watrTemp: tmp[0]})
-        res.json(tmp[0])
-        await browser.close()
-    }
+app.get("/result", (req, res) => {
+  // async function scrapeTemperature() {
+  //   const browser = await launch({ args: ["--no-sandbox"] });
+  //   const page = await browser.newPage();
+  //   await page.goto(
+  //     "https://tides4fishing.com/au/new-south-wales/merimbula#_water_temp"
+  //   );
 
-    scrapeTemperature()
-})
+  //   const tmp = await page.evaluate(() => {
+  //     return Array.from(
+  //       document.querySelectorAll(
+  //         "#c_grafico_temp_agua_txt_grados_agua_actual strong"
+  //       )
+  //     ).map((x) => x.textContent);
+  //   });
 
-app.get('/tides', (req, res) => {
+  //console.log({watrTemp: tmp[0]})
+  // res.json("tmp[0]");
+  // await browser.close();
+  // }
 
-    async function scrapeTides() {
-        const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-        const page = await browser.newPage();
-        await page.goto('https://www.tidetime.org/australia-pacific/australia/merimbula.htm');
+  // scrapeTemperature();
+  res.json("Not available");
+});
 
-        const tide = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll('#tides-today > p')).map(x => x.textContent)
-        });
+app.get("/tides", async (req, res) => {
+  //   async function scrapeTides() {
+  //     const browser = await launch({ args: ["--no-sandbox"] });
+  //     const page = await browser.newPage();
+  //     await page.goto(
+  //       "https://www.tidetime.org/australia-pacific/australia/merimbula.htm"
+  //     );
 
-        
-        //console.log(tide[0])
-        res.json(tide[0])
-        await browser.close()
-    }
+  //     const tide = await page.evaluate(() => {
+  //       return Array.from(document.querySelectorAll("#tides-today > p")).map(
+  //         (x) => x.textContent
+  //       );
+  //     });
 
-    scrapeTides()
+  //console.log(tide[0])
+  //   res.json(tide[0]);
+  // await browser.close();
+  //   }
 
-})
+  //   scrapeTides();
+  res.json("Not available");
+});
 
-app.listen(process.env.PORT || 3080, () => console.log(`server running on PORT ${process.env.PORT || 3080}`))
+app.listen(process.env.PORT || 3080, () =>
+  console.log(`Server running on PORT ${process.env.PORT || 3080}`)
+);
